1ae793d32e7be1ccc8ad96efb11afad8f434d55d
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-20 22:09:24 +0600

    Renamed User Route

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// @route    GET api/users
+// @route    GET api/user
 // @desc     Test Users Route
 // @access   Public
-router.get("/", (req, res) => res.send("Test Users Route"));
+router.get("/", (req, res) => res.send("Test User Route"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 app.get("/", (req, res) => res.send("BEATS VERSE IO API is running..."));
 
 // Define Routes
-app.use("/api/users", require("./routes/api/users"));
+app.use("/api/user", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 
