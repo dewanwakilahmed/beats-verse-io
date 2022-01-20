5cef0a7c49c759264f6078dcb4f43ff8dfffe655
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-21 01:25:19 +0600

    Added Custom Auth Middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+  // Get Token from Header
+  const token = req.header("x-auth-token");
+
+  // Check if Token is send
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: "No Token Provided. Authorization Denied!" });
+  }
+
+  // Verify Token
+  try {
+    const decodedToken = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decodedToken.user;
+  } catch (error) {
+    console.error(error.message);
+    res.status(401).json({ msg: "Provided Token is not Valid!" });
+  }
+};
